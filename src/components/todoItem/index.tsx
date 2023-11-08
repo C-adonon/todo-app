376ba84f54d448cf45317748c62042a2f8f07688
Chloé <cadonon.dev@gmail.com>
2023-11-08 15:01:35 +0100

    modified checked into default checked

--- src/components/todoItem/index.tsx
+++ src/components/todoItem/index.tsx
@@ -3,11 +3,10 @@ import './todoItem.css'
 
 /* component */
 function TodoItem(props) {
-
   if (props.status) {
     return (
       <li className="todoItem">
-        <input type="checkbox" checked />
+        <input type="checkbox" defaultChecked />
         <p>{props.todo}</p>
         <button onClick={() => props.onDelete(props.id)} >Supprimer</button>
       </li>
