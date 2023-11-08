b6a11c0659a41f4a70f21373cd5462a88df145e0
Chloé <cadonon.dev@gmail.com>
2023-11-08 15:31:40 +0100

    created filterbar

---
+++ src/components/filterBar/index.tsx
@@ -0,0 +1,13 @@
+// zone d'import
+import { useState } from 'react'
+import './addTodoItem.css'
+
+// component 
+function FilterBar() {
+    return (
+        <div className="filter-bar">
+        </div>
+    )
+}
+
+export default FilterBar;
