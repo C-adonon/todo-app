25216372fe98b5502ebfc25614143818372b3baa
Chloé <cadonon.dev@gmail.com>
2023-11-15 13:06:59 +0100

    added filter mngt

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './assets/App.css'
 import TodoList from './components/todoList'
 import AddTodoItem from './components/addTodoItem'
-
+import FilterBar from './components/filterBar'
+let count = 4;
 function App() {
   const [todos, setTodos] = useState([
-    { value: 'Todo 1', completed: false },
-    { value: 'Todo 2', completed: true },
-    { value: 'Todo 3', completed: false },
+    { id: 1, value: 'Todo 1', completed: false },
+    { id: 2, value: 'Todo 2', completed: true },
+    { id: 3, value: 'Todo 3', completed: false },
   ])
- 
+
+  const [filterBy, setFilterBy] = useState("all")
+
   function handleDeleteTodoItem(id: number) {
     let newTodos = [...todos]
     newTodos.splice(id, 1)
@@ -18,15 +21,35 @@ function App() {
 
   function handleAddTodoItem(value: string) {
     let newTodos = [...todos]
-    newTodos.push({ value: value, completed: false })
+    newTodos.push({ id: count++, value: value, completed: false })
     setTodos(newTodos)
   }
+
+  function handleFilter(status) {
+    console.log(status)
+    if (status == null) {
+      setFilterBy("all")
+    } else if (status == true) {
+      setFilterBy("completed")
+    } else if (status == false) {
+      setFilterBy("not-completed")
+    }
+  }
+
+  let filteredTodos = todos.filter((todo) => {
+    if (filterBy == "all") return true
+    else if (filterBy == "completed") return todo.completed == true
+    else if (filterBy == "not-completed") return todo.completed == false
+  })
+
+  console.log(filteredTodos)
 
   return (
     <div className='App'>
       <h1>TODO LIST</h1>
       <AddTodoItem onAdd={handleAddTodoItem} />
-      <TodoList todos={todos} handleDeleteTodoItem={handleDeleteTodoItem} />
+      <TodoList todos={filteredTodos} handleDeleteTodoItem={handleDeleteTodoItem} />
+      <FilterBar onFilter={handleFilter} />
     </div>
   )
 }
